81b1b6499196d765add42d928fc1588a4150c306
Heruwim <heruwim25@gmail.com>
2024-03-28 16:14:40 +0200

    Added a cell check when a match is made

--- Assets/Scripts/Board/BoardService.cs
+++ Assets/Scripts/Board/BoardService.cs
@@ -1,6 +1,5 @@
 using StaticData;
-using System;
-using UnityEditor.U2D.Aseprite;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -11,6 +10,7 @@ public class BoardService : MonoBehaviour
 
     private CellData[,] _boards;
     private CellFactory _cellFactory;
+    private MatchMachine _matchMachine;
 
     public ArrayLayout BoartLayout;
 
@@ -19,15 +19,65 @@ public class BoardService : MonoBehaviour
     private void Awake()
     {
         _cellFactory = GetComponent<CellFactory>();
+        _matchMachine = new MatchMachine(this);
     }
 
     private void Start()
     {
         InitializeBoard();
+        VerifyBoardOnMatches();
         _cellFactory.InstantiateBoard(this);
     }
 
-    
+    private void VerifyBoardOnMatches()
+    {
+        for (int y = 0; y < Config.BoardHeight; y++)
+        {
+            for (int x = 0; x < Config.BoardWidth; x++)
+            {
+                Point point = new Point(x, y);
+                var cellTypeAtPoint = GetCellTypeAtPoint(point);
+                if(cellTypeAtPoint <= 0)
+                {
+                    continue;
+                }
+
+                var removeCellTypes = new List<CellData.CellType>();
+                while (_matchMachine.GetMatchedPoints(point, true).Count > 0)
+                {
+                    if(removeCellTypes.Contains(cellTypeAtPoint) == false)
+                    {
+                        removeCellTypes.Add(cellTypeAtPoint);
+                    }
+                    SetCellTypeAtPoint(point, GetNewCellType(ref removeCellTypes));
+                    
+                } 
+            }
+        }
+    }
+
+    private void SetCellTypeAtPoint(Point point, CellData.CellType newCellType)
+    {
+        _boards[point.X, point.Y].NewCellType = newCellType;
+    }
+
+    private CellData.CellType GetNewCellType(ref List<CellData.CellType> removeCellTypes)
+    {
+        var availableCellTypes = new List<CellData.CellType>();
+        for (int i = 0; i < CellSprites.Length; i++)
+        {
+            availableCellTypes.Add((CellData.CellType)i + 1);
+        }
+        foreach (var removeCellType in removeCellTypes)
+        {
+            availableCellTypes.Remove(removeCellType);
+        }
+
+        return availableCellTypes.Count <= 0 ? CellData.CellType.Blank :
+            availableCellTypes[Random.Range(0, availableCellTypes.Count)];
+    }
+
+    private CellData.CellType GetCellTypeAtPoint(Point point) => _boards[point.X, point.Y].NewCellType;
 
     private void InitializeBoard()
     {
---
+++ Assets/Scripts/MatchMachine/MatchMachine.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+public class MatchMachine
+{
+    private readonly BoardService _boardService;
+
+    public MatchMachine(BoardService boardService)
+    {
+        _boardService = boardService;
+    }
+
+    public List<Point> GetMatchedPoints(Point point, bool main)
+    {
+        return null;
+    }
+}
