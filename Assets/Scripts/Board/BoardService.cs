3b616871968f3f69c500927257e368f6d1c270d4
Heruwim <heruwim25@gmail.com>
2024-03-28 11:26:41 +0200

    Refactoring code and adding a factory for cells

--- Assets/Scripts/Board/BoardService.cs
+++ Assets/Scripts/Board/BoardService.cs
@@ -4,45 +4,30 @@ using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(CellFactory))]
 public class BoardService : MonoBehaviour
 {
-    [SerializeField] private RectTransform _BoardRect;
-    [SerializeField] private Cell _cellPrefab;
     [SerializeField] private Sprite[] _cellSprites;
 
     private CellData[,] _boards;
+    private CellFactory _cellFactory;
 
     public ArrayLayout BoartLayout;
+
+    public Sprite[] CellSprites => _cellSprites;
+
+    private void Awake()
+    {
+        _cellFactory = GetComponent<CellFactory>();
+    }
 
     private void Start()
     {
         InitializeBoard();
-        InstantiateBoard();
+        _cellFactory.InstantiateBoard(this);
     }
 
-    private void InstantiateBoard()
-    {
-        for (int y = 0; y < Config.BoardHeight; y++)
-        {
-            for (int x = 0; x < Config.BoardWidth; x++)
-            {
-                Point point = new Point(x, y);
-                CellData cellData = GetCellAtPoint(point);
-                var cellType = cellData.NewCellType;
-                if(cellType <= 0)
-                {
-                    continue;
-                }
-
-                Cell cell = InstantiateCell();
-                cell.Rect.anchoredPosition = GetBoardPositionFromPoint(point);
-                cell.Initialize(cellData, _cellSprites[(int)(cellType - 1)]);
-
-            }
-        }
-    }
-
-    private CellData GetCellAtPoint(Point point) => _boards[point.X, point.Y];
+    
 
     private void InitializeBoard()
     {
@@ -58,9 +43,9 @@ public class BoardService : MonoBehaviour
 
     private CellData.CellType GetRandomCellType() => (CellData.CellType)(Random.Range(0, _cellSprites.Length) + 1);
 
-    private Cell InstantiateCell() => Instantiate(_cellPrefab, _BoardRect);
+    public CellData GetCellAtPoint(Point point) => _boards[point.X, point.Y];
 
-    private Vector2 GetBoardPositionFromPoint(Point point)
+    public Vector2 GetBoardPositionFromPoint(Point point)
     {
         return new Vector2(Config.PieceSize / 2 + Config.PieceSize * point.X, -Config.PieceSize / 2 - Config.PieceSize * point.Y);
     }
---
+++ Assets/Scripts/Board/CellFactory.cs
@@ -0,0 +1,34 @@
+﻿using StaticData;
+using UnityEngine;
+
+public class CellFactory : MonoBehaviour
+{
+    [SerializeField] private RectTransform _BoardRect;
+    [SerializeField] private Cell _cellPrefab;
+
+    private BoardService _boardService;
+
+    public void InstantiateBoard(BoardService boardService)
+    {
+        for (int y = 0; y < Config.BoardHeight; y++)
+        {
+            for (int x = 0; x < Config.BoardWidth; x++)
+            {
+                Point point = new Point(x, y);
+                CellData cellData = boardService.GetCellAtPoint(point);
+                var cellType = cellData.NewCellType;
+                if (cellType <= 0)
+                {
+                    continue;
+                }
+
+                Cell cell = InstantiateCell();
+                cell.Rect.anchoredPosition = boardService.GetBoardPositionFromPoint(point);
+                cell.Initialize(cellData, boardService.CellSprites[(int)(cellType - 1)]);
+
+            }
+        }
+    }    
+
+    private Cell InstantiateCell() => Instantiate(_cellPrefab, _BoardRect);
+}
